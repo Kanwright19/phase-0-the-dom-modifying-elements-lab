02eaced693dd2686bf0b26eb4b293789f7ed8833
Kairi Wright <kanwright19@gmail.com>
2023-12-07 16:33:09 -0500

    DOM Modifying Elements

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const main1 = document.querySelector('main#main')
+console.log(main1)
+
+main1.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = 'victory';
+newHeader.getElementById = 'victory';
+
+newHeader.innerHTML = 'Kairi is the champion';
